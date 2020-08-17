99cf0006236d44b1b16fe5c265a56bf02921a713
Prycey77 <rich_pryce@yahoo.co.uk>
2020-08-17 20:57:59 +0100

    added methods to directory.rb

--- directory.rb
+++ directory.rb
@@ -6,10 +6,19 @@ students = [
   "Nurse Ratched",
   "Norman Bates"
 ]
-
-puts "The students of Villains Academy"
-puts "--------------------------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
 end
-puts "Overall we have #{students.length} great students"
+def print(names)
+  names.each do |student|
+  puts student
+  end
+end
+def print_footer(names)
+puts "Overall we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
