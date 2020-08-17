0809b69403028eb579a09603096fb7695cd43747
Prycey77 <rich_pryce@yahoo.co.uk>
2020-08-17 20:39:42 +0100

    directory edited

--- directory.rb
+++ directory.rb
@@ -1,6 +1,10 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "--------------------------------"
 puts "Dr. Hannibal Lecter"
 puts "Darth Vader"
 puts "Nurse Ratched"
 puts "Norman Bates"
+print "Overall we have "
+print 4
+puts " great students"
